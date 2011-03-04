784808d7719226c5edd8d1225aa31cd9f14bd20c
Raul Riera <rieraraul@gmail.com>
2011-03-04 13:39:09 +0100

    Replaced Chuck with Beatrix

--- Resources/views/celebrity_answers.js
+++ Resources/views/celebrity_answers.js
@@ -1,4 +1,4 @@
 var possible_answers = [];
 possible_answers[0] = [L("celebrity_0_possible_answer_1"), L("celebrity_0_possible_answer_2"), L("celebrity_0_possible_answer_3")];
-possible_answers[1] = [L("celebrity_1_possible_answer_1"), L("celebrity_1_possible_answer_2")];
+possible_answers[1] = [L("celebrity_1_possible_answer_1"), L("celebrity_1_possible_answer_2"), L("celebrity_1_possible_answer_3")];
 possible_answers[2] = [L("celebrity_2_possible_answer_1"), L("celebrity_2_possible_answer_2")];
