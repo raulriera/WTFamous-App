68a3803b1a66f4b72d8017a7f789801c32d7b391
Raul Riera <rieraraul@gmail.com>
2011-03-14 11:45:04 +0100

    Added character "Winner"

--- Resources/views/celebrity_answers.js
+++ Resources/views/celebrity_answers.js
@@ -1,4 +1,9 @@
 var possible_answers = [];
+// sexy internet man
 possible_answers[0] = [L("celebrity_0_possible_answer_1"), L("celebrity_0_possible_answer_2"), L("celebrity_0_possible_answer_3")];
+// assassin
 possible_answers[1] = [L("celebrity_1_possible_answer_1"), L("celebrity_1_possible_answer_2"), L("celebrity_1_possible_answer_3")];
+// luis chataing
 possible_answers[2] = [L("celebrity_2_possible_answer_1"), L("celebrity_2_possible_answer_2")];
+// winner
+possible_answers[3] = [L("celebrity_3_possible_answer_1"), L("celebrity_3_possible_answer_2"), L("celebrity_3_possible_answer_3"), L("celebrity_3_possible_answer_4")];
