f683753ba5b3160e68569f051caa0a2ea9819c77
Raul Riera <rieraraul@gmail.com>
2011-04-08 02:25:48 +0200

    Removed "Winner" and "El President" as the AppStore requested

--- Resources/views/celebrity_answers.js
+++ Resources/views/celebrity_answers.js
@@ -5,7 +5,3 @@ possible_answers[0] = [L("celebrity_0_possible_answer_1"), L("celebrity_0_possib
 possible_answers[1] = [L("celebrity_1_possible_answer_1"), L("celebrity_1_possible_answer_2"), L("celebrity_1_possible_answer_3")];
 // luis chataing
 possible_answers[2] = [L("celebrity_2_possible_answer_1"), L("celebrity_2_possible_answer_2")];
-// winner
-possible_answers[3] = [L("celebrity_3_possible_answer_1"), L("celebrity_3_possible_answer_2"), L("celebrity_3_possible_answer_3"), L("celebrity_3_possible_answer_4")];
-// el presidente
-possible_answers[4] = [L("celebrity_4_possible_answer_1"), L("celebrity_4_possible_answer_2"), L("celebrity_4_possible_answer_3"), L("celebrity_4_possible_answer_4")];
